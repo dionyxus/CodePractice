92bebcea80353c245aa7008fa772e177bcb9f09e
dionyxus <asingh309@mylangara.ca>
2023-09-06 15:15:18 -0700

    The Birthday Bar
    Easy subarray

---
+++ Algorithms/TheBirthdayBar.js
@@ -0,0 +1,18 @@
+'use strict';
+
+let si = [4];
+let di = 4;
+let mi = 1;
+
+function birthday(s, d, m) {
+    let ways = 0;
+
+    for(let i = 0 ; i <= s.length - m ; i++){
+        let pair = s.slice(i,i + m);
+        pair.reduce((a, b) => a + b, 0) === d ? ways++ : {};
+    }
+
+    return ways;
+}
+
+console.log(birthday(si,di,mi))
